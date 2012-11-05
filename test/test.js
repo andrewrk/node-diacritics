149bc2c797f4fbc85838b830c5daf63a2897fcf1
Andrew Kelley <superjoe30@gmail.com>
2012-11-05 13:15:29 -0500

    more diacritics

--- test/test.js
+++ test/test.js
@@ -3,3 +3,4 @@ var removeDiacritics = require('../').remove,
 
 assert.strictEqual(removeDiacritics("Iлｔèｒｎåｔïｏｎɑｌíƶａｔï߀ԉ"), "internationalization");
 assert.strictEqual(removeDiacritics("Båｃòл íｐѕùｍ ԁｏɭ߀ｒ ѕïｔ ａϻèｔ âùｔê ａԉᏧ߀üïｌɭê ƃëéｆ ｃｕｌρá ｆïｌèｔ ϻｉǥｎòｎ ｃｕρｉᏧａｔａｔ ｕｔ êлｉｍ ｔòлɢùê."), "bacon ipѕum dolor ѕit amet aute andouille beef culpa filet mignon cupidatat ut enim tongue.");
+assert.strictEqual(removeDiacritics("ᴎᴑᴅᴇȷʂ"), "nodejs");
