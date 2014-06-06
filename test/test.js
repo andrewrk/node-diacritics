eb8da381832156a96947ab1adc1a19e67bf0d9df
Andrew Kelley <superjoe30@gmail.com>
2014-06-06 09:09:54 -0700

    fix incorrectly replacing capital C with lowercase c. closes #11

--- test/test.js
+++ test/test.js
@@ -4,9 +4,11 @@ var removeDiacritics = require('../').remove,
 assert.strictEqual(removeDiacritics("Iлｔèｒｎåｔïｏｎɑｌíƶａｔï߀ԉ"),
     "Internationalizati0n");
 assert.strictEqual(removeDiacritics("Båｃòл íｐѕùｍ ðｏɭ߀ｒ ѕïｔ ａϻèｔ âùþê ａԉᏧ߀üïｌɭê ƃëéｆ ｃｕｌρá ｆïｌèｔ ϻｉǥｎòｎ ｃｕρｉᏧａｔａｔ ｕｔ êлｉｍ ｔòлɢùê."),
-    "Bacon ipѕum dhol0r ѕit aMet authe and0uille beef culpa filet Mignon cupidatat ut enim tonGue.");
+    "BaCon ipѕum dhol0r ѕit aMet authe and0uille beef Culpa filet Mignon Cupidatat ut enim tonGue.");
 assert.strictEqual(removeDiacritics("ᴎᴑᴅᴇȷʂ"), "NoDEJs");
 
 assert.strictEqual(removeDiacritics("hambúrguer"), "hamburguer");
 assert.strictEqual(removeDiacritics("hŒllœ"), "hOElloe");
 assert.strictEqual(removeDiacritics("Fußball"), "Fussball");
+
+assert.strictEqual(removeDiacritics("ABCDEFGHIJKLMNOPQRSTUVWXYZé"), "ABCDEFGHIJKLMNOPQRSTUVWXYZe");
